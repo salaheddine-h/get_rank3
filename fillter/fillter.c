29f2b4d558b3bd2ecb9a960b467ff22a5dd600fe
salaheddine <salaheddineh7@gmail.com>
2025-07-08 16:04:35 +0100

    push

--- fillter/fillter.c
+++ fillter/fillter.c
@@ -1,205 +1,74 @@
-// #include <unistd.h>
-// #include <stddef.h>
-// #include <stdio.h>
-
-// #define BUFFER_SIZE 1024
-
-// int	ft_strlen(char *str)
-// {
-// 	int	i = 0;
-// 	while(str[i])
-// 		i++;
-// 	return(i);
-// }
-
-// void	replace_src(char *str, int replace_len)
-// {
-// 	int	i = 0;
-// 	while(i < replace_len)
-// 	{
-// 		str[i] = '*';
-// 		i++;
-// 	}
-// }
-
-// int	check_replace(char *str, char *replace)
-// {
-// 	int	i = 0;
-
-// 	while(str[i] && replace[i])
-// 	{
-// 		if(str[i] && str[i] != replace[i])
-// 			return(0);
-// 		i++;
-// 	}
-// 	return(1);
-// }
-// void	filltring(char	*str, char *replace, int src_len)
-// {
-// 	int	replace_len = ft_strlen(replace);
-// 	int	i = 0;
-
-// 	while(str[i])
-// 	{
-// 		if(check_replace(&str[i], replace))
-// 		{
-// 			replace_src(&str[i], replace_len);
-// 			i += replace_len;
-// 		}
-// 		else
-// 			i++;
-// 	}
-
-// }
-// int	main(int argc, char **argv)
-// {
-// 	char	buffer_read[BUFFER_SIZE + 1];
-// 	ssize_t	read_byte;
-
-// 	if(argc != 2)
-// 		return(1);
-// 	while(1)
-// 	{
-// 		read_byte = read(0, buffer_read, BUFFER_SIZE);
-// 		if(read_byte > 0)
-// 		{
-// 			filltring(buffer_read, argv[1], read_byte);
-// 			write(1, buffer_read, read_byte);
-// 		}
-// 		else if(read_byte == 0)
-// 			break;
-// 		else
-// 		{
-// 			write(1, "Error: ", 7);
-// 			perror("");
-// 			return(1);
-// 		}
-// 	}
-// 	return(0);
-// }
-
-
-
-
 #include <unistd.h>
-#include <stdlib.h>
-#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int	ft_strlen(char *str)
+char	*read_input(void)
 {
-	int	i = 0;
-	while(str[i])
-		i++;
-	return(i);
-}
-char	*ft_strdup(char *str)
-{
-	int	i = 0;
-	while(str[i])
-		i++;
-	char	*ptr = malloc(i + 1 *(sizeof(char)));
-	if(!ptr)
-		return(NULL);
-	i = 0;
-	while(str[i])
+	char	*buff = malloc(1), tmp;
+	int	n;
+	int	len = 0;
+
+	if(!buff)
 	{
-		ptr[i] = str[i];
-		i++;
+		perror("Error: ");
+		return(NULL);
 	}
-	ptr[i] = '\0';
-	return(ptr);
-}
-
-char	*get_next_line(int fd)
-{
-	static char buffer_read[BUFFER_SIZE];
-	static char	line[70000];
-	static int	read_num;
-	static int	copy_pos;
-	int	i = 0;
-
-	if(fd < 0 || BUFFER_SIZE < 0)
-		return(NULL);
-	while(1)
+	while((n = read(0, &tmp, 1)) > 0)
 	{
-		if(copy_pos >= read_num)
+		char	*new_buffer = realloc(buff, len + 2);
+		if(!new_buffer)
 		{
-			read_num = read(fd, buffer_read, BUFFER_SIZE);
-			copy_pos = 0;
-			if(read_num < 0)
-				break;
+			free(buff);
+			perror("Error: ");
+			return(NULL);
 		}
-		line[i++] = buffer_read[copy_pos++];
-		if(line[i - 1] == '\n')
-			break;
+		buff = new_buffer;
+		buff[len++] = tmp;
 	}
-	line[i] = '\0';
-	if(i == 0)
+	if(n < 0)
+	{
+		free(buff);
+		perror("Error: ");
 		return(NULL);
-	return(ft_strdup(line));
+	}
+	buff[len] = '\0';
+	if(len > 0 && buff[len - 1] == '\n')
+		buff[len - 1] = '\0';
+	return(buff);
 }
 
-int	check_replace(char *str, char *replace)
+void	replace(char *str, char *s)
 {
+	int	len = strlen(s);
 	int	i = 0;
-	while (str[i] && replace[i])
-	{
-		if(str[i] && str[i] != replace[i])
-			return(0);
-		i++;
-	}
-	return(0);
-}
-void	filltring(char	*str, char *replace, int src_len)
-{
-	int	replace_len = ft_strlen(replace);
-	int	i = 0;
+	int	j;
 
 	while(str[i])
 	{
-		if(check_replace(&str[i], replace))
+		for(j = 0; s[j] && str[i + j] == s[j]; j++);
+		if(j == len)
 		{
-			replace_src(&str[i], replace_len);
-			i += replace_len;
+			for(j = 0; j < len; j++)
+				str[i + j] = '*';
+			i += len;
 		}
 		else
 			i++;
 	}
 }
 
-void	replace_src(char *str, int replace_len)
-{
-	int	i = 0;
-	while(i < replace_len)
-	{
-		str[i] = '*';
-		i++;
-	}
-}
 int	main(int argc, char **argv)
 {
-	static char	buffer_read[BUFFER_SIZE + 1];
-	ssize_t	read_byte;
-	int	i = 0;
-	if(argc != 2)
+	char	*buffer_read;
+
+	if(argc != 2 || !argv[1][0])
 		return(1);
-	while(1)
-	{
-		read_byte = get_next_line(0);
-		if(read_byte >= 0)
-		{
-			filltring(buffer_read, argv[1], read_byte);
-			write(1, buffer_read, read_byte);
-		}
-		else if(read_byte == 0)
-			break;
-		else
-		{
-			write(1, "Error: ", 7);
-			perror("");
-			return(1);
-		}
-	}
+	buffer_read = read_input();
+	if(!buffer_read)
+		return(1);
+	replace(buffer_read, argv[1]);
+	printf("%s\n ", buffer_read);
+	free(buffer_read);
 	return(0);
 }
