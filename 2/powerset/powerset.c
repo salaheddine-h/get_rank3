6208deeb96a9e77f404312c46f58a9ea847e726d
Salah Eddine Hali <salhali@c4r9p4.1337.ma>
2025-08-19 13:53:13 +0100

    fixe powerset_valid

--- 2/powerset/powerset.c
+++ 2/powerset/powerset.c
@@ -1,49 +1,50 @@
-//shortest version:
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-void find_subsets(int *set, int size, int target, int *subset, int subsize, int idx)
+void	find_subset(int *set, int size, int target, int *subset, int subsize, int idx)
 {
-	if (idx == size)
+	if(idx == size)
 	{
 		int sum = 0;
-		for (int i = 0; i < subsize; i++)
+		for(int i = 0; i < subsize; i++)
 			sum += subset[i];
-		if (sum == target)
+		if(sum == target)
 		{
-			for (int i = 0; i < subsize; i++)
-				printf("%d ", subset[i]);
+			for(int i = 0; i < subsize; i++)
+			{
+				if(i > 0)
+					printf(" ");
+				printf("%d", subset[i]);
+			}
 			printf("\n");
 		}
 		return;
 	}
+	find_subset(set, size, target, subset, subsize,  idx + 1);
 	subset[subsize] = set[idx];
-	find_subsets(set, size, target, subset, subsize + 1, idx + 1);
-	find_subsets(set, size, target, subset, subsize, idx + 1);
+	find_subset(set, size, target, subset, subsize + 1, idx + 1);
+
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	if (argc < 3)
-    return 1;
+	if(argc < 3)
+		return(1);
 	int target = atoi(argv[1]);
 	int size = argc - 2;
-	int *set = malloc(size * sizeof(int));
-	int *subset = malloc(size * sizeof(int));
-	if (!set || !subset)
-  {
-    free(set),
-    free(subset);
-    return 1;
-  }
-
-	for (int i = 0; i < size; i++)
+	int *subset = malloc(sizeof(int) * size);
+	int *set = malloc(sizeof(int) * size);
+	if(!set || !subset)
+	{
+		free(set);
+		free(subset);
+		return(1);
+	}
+	for(int i = 0; i < size; i++)
 		set[i] = atoi(argv[i + 2]);
-	find_subsets(set, size, target, subset, 0, 0);
+	find_subset(set, size, target, subset, 0, 0);
 	free(set);
-  free(subset);
-	return 0;
+	free(subset);
+	return(0);
 }
-
-gcc -Wall -Werror -Wextra -o powerset powerset.c
-valgrind --leak-check=full ./powerset 5 1 2 3 4 5
-
-
